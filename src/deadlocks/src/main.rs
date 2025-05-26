18ac56ac93461013dd568e6b24191b7d2ad6a416
michal-hynek <hynek.michal@gmail.com>
2025-05-26 08:48:52 -0700

    Deadlock examples

---
+++ src/deadlocks/src/main.rs
@@ -0,0 +1,25 @@
+use std::sync::Mutex;
+
+fn main() {
+    // deadlock - the second lock() call will wait until the previous lock goes out of scope
+    /*let my_shared = Mutex::new(0);
+    let lock = my_shared.lock().unwrap();
+    let lock = my_shared.lock().unwrap();*/
+
+    // no deadllock becase the inner lock goes out of scope before the next lock
+    /*let my_shared = Mutex::new(0);
+    {
+        let lock = my_shared.lock().unwrap();
+    }
+
+    let lock = my_shared.lock().unwrap();*/
+
+    let my_shared = Mutex::new(0);
+    let lock = my_shared.lock().unwrap();
+
+    if let Ok(lock) = my_shared.try_lock() {
+        println!("got the lock");
+    } else {
+        println!("getting the lock failed");
+    }
+}
