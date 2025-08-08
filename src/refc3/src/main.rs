596cfb654bd1a917267fa10afa57ba6e36681d73
michal-hynek <hynek.michal@gmail.com>
2025-08-08 08:47:41 -0700

    Mutex with shared references using Arc

---
+++ src/refc3/src/main.rs
@@ -0,0 +1,24 @@
+use std::sync::{Arc, Mutex};
+
+struct SharedData(String);
+
+fn main() {
+    let my_shared = Arc::new(Mutex::new(SharedData("Hello".to_string())));
+    let mut threads = Vec::with_capacity(10);
+
+    for i in 0..10 {
+        let my_shared = my_shared.clone();
+
+        threads.push(std::thread::spawn(move || {
+            let mut data = my_shared.lock().unwrap();
+            data.0.push_str(&format!(" {i}"));
+        }));
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
+
+    let data = my_shared.lock().unwrap();
+    println!("{}", data.0);
+}
