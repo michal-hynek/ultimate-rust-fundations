73e041fa58752ca55404380d55ebee9dc7364b20
michal-hynek <hynek.michal@gmail.com>
2025-08-20 07:00:13 -0700

    Custom iterator with lifetimes

---
+++ src/iterators2/src/main.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+
+struct HashMapBucket<K,V> {
+    map: HashMap<K, Vec<V>>,
+}
+
+impl<K,V> HashMapBucket<K,V>
+where K: Eq + std::hash::Hash
+{
+    fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, key: K, value: V) {
+        let values = self.map.entry(key).or_default();
+        values.push(value);
+    }
+}
+
+impl <K,V> HashMapBucket<K, V> {
+    fn iter(&self) -> HashMapBucketIter<K, V> {
+        let mut key_iter = self.map.iter();
+        let current_map_entry = key_iter.next();
+        HashMapBucketIter {
+            key_iter,
+            current_map_entry,
+            current_vec_index: 0,
+        }
+    }
+}
+
+struct HashMapBucketIter<'a, K, V> {
+    key_iter: std::collections::hash_map::Iter<'a, K, Vec<V>>,
+    current_map_entry: Option<(&'a K, &'a Vec<V>)>,
+    current_vec_index: usize,
+}
+
+// Specify 'a - the lifetime, and K,V on both sides.
+// If you wanted to change how the iterator acts for a given type of key or
+// value you could cange the left-hand side.
+impl <'a, K, V> Iterator for HashMapBucketIter<'a, K, V> {
+    // Define `Item` - a type used inside the trait - to be a reference to a key and value.
+    // This specifies the type that the iterator will return.
+    type Item = (&'a K, &'a V);
+
+    // You use Item to specify the type returned by `Next`. It's always an option of the type.
+    fn next(&mut self) -> Option<Self::Item> {
+        // If there is a current map entry, and a current vec index
+        if let Some((key, values)) = self.current_map_entry {
+            // If the index is less than the length of the vector
+            if self.current_vec_index < values.len() {
+                // Get the value at the current index
+                let value = &values[self.current_vec_index];
+                // Increment the index
+                self.current_vec_index += 1;
+                // Return the key and value
+                return Some((key, value));
+            } else {
+                // We're past the end of the vector, next key
+                self.current_map_entry = self.key_iter.next();
+                self.current_vec_index = 0;
+
+                if let Some((key, values)) = self.current_map_entry {
+                    // If the index is less than the length of the vector
+                    if self.current_vec_index < values.len() {
+                        // Get the value at the current index
+                        let value = &values[self.current_vec_index];
+                        // Increment the index
+                        self.current_vec_index += 1;
+                        // Return the key and value
+                        return Some((key, value));
+                    }
+                }
+            }
+        }
+
+        None
+    }
+}
+
+fn main() {
+    let mut my_buckets = HashMapBucket::new();
+    my_buckets.insert("hello", 1);
+    my_buckets.insert("hello", 2);
+    my_buckets.insert("goodbye", 3);
+
+    for (key, val) in my_buckets.iter() {
+        println!("{key}, {val:?}");
+    }
+}
